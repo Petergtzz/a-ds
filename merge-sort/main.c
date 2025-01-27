c217e92defbf364e8b51b9752290856bc3fde4c0
Pedro <peter.gtz2020@gmail.com>
2025-01-26 19:19:47 -0700

    moved xor to roor dir and defined macro for xor

--- merge-sort/main.c
+++ merge-sort/main.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
-#include "../xorshift.c"
 
-// Insertion Sort Algorithm
-// Input  a: a[a,...,n-1]
-// Output b: b[a,...,n-1]
-// b is same as a, but sorted
+// Merge Sort Algorithm
+// Merge(a, b)
+// Does not matter is a or b are sorted 
 
 // Time Complexity:
 //  Best Case: Array is already sorted O(n)
 //  Worst Case: Array is in asc to dec order O(n^2)
 
 #define MAX_SIZE 10
-#define rand(state, max) xorshift32(state, max)
 
 typedef struct {
     int32_t items[MAX_SIZE];
@@ -20,29 +17,6 @@ typedef struct {
     // int32_t capacity;
 } Int32Array;
 
-void swap(int32_t *x, int32_t *y)
-{    
-    int32_t temp = *x;
-    *x = *y;
-    *y = temp; 
-}
-
-void insertion_sort(Int32Array *input)
-{
-    // Loop over all a
-    for(int i = 1; i < MAX_SIZE; i++)
-    {
-        // Assign ith element to j 
-        int32_t j = i;
-        // While j > 0 and j < i-1
-        while(j > 0 && input->items[j] < input->items[j-1])
-        {
-            // Swap numbers
-            swap(&input->items[j], &input->items[j-1]);
-            j--;
-        }
-    }
-}
 
 int main(int argc, char **argv)
 {
@@ -54,7 +28,7 @@ int main(int argc, char **argv)
     printf("Unsorted Array:\n");
     for(int i = 0; i < MAX_SIZE; i++)
     {
-        int32_t number = rand(&state, 100);
+        int32_t number = xorshift32(&state);
         input.items[i] = number;
         printf("%d\n", input.items[i]);
     }
--- xorshift.c
+++ xorshift.c
@@ -4,12 +4,12 @@ typedef struct {
     int32_t a;
 } XorShift32_state;
 
-int32_t xorshift32(XorShift32_state *state)
+int32_t xorshift32(XorShift32_state *state, int32_t max)
 {
 	/* Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs" */
 	int32_t x = state->a;
 	x ^= x << 13;
 	x ^= x >> 17;
 	x ^= x << 5;
-	return state->a = x;
+	return state->a = (x % max);
 }
